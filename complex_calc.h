cb9acab2728fa27a0603f885a3dfcfb5a5f0a8da
Alexey Stolpovsky <alex.stolpovsky@yandex.ru>
2016-07-11 16:30:04 +0600

    initial project version

---
+++ complex_calc.h
@@ -0,0 +1,10 @@
+typedef struct {
+	float r;
+	float i;
+} complex_t;
+
+complex_t cplx_add(complex_t a, complex_t b);
+complex_t cplx_sub(complex_t a, complex_t b);
+complex_t cplx_mul(complex_t a, complex_t b);
+complex_t cplx_div(complex_t a, complex_t b);
+
